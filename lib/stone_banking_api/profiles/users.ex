130e01a456894c4973cecb5bf56518ff28e312da
Vinicius M.R. de Carvalho <vinicius.carvalho@stone.com.br>
2021-04-14 00:12:04 -0300

    feat: create account automatically

--- lib/stone_banking_api/profiles/users.ex
+++ lib/stone_banking_api/profiles/users.ex
@@ -2,12 +2,32 @@ defmodule StoneBankingAPI.Profiles.Users do
   @moduledoc """
   Module for handling users database queries
   """
+  alias Ecto.Multi
   alias StoneBankingAPI.Repo
   alias StoneBankingAPI.Profiles.Schemas.User
+  alias StoneBankingAPI.Accounts.Schemas.Account
 
+  @doc """
+  Creates an user and his bank account with R$1000.
+  Returns an user if succesfull or a changeset if the operation fails.
+  """
+  @spec create(map()) :: {:ok, User.t()} | {:error, Ecto.Changeset.t()}
   def create(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(params))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
+      Account.changeset(%{user_id: user.id})
+      |> repo.insert
+    end)
+    |> Repo.transaction()
+    |> handle_multi()
+  end
+
+  defp handle_multi({:error, _failed_operation, changeset, _changes_so_far}) do
+    {:error, changeset}
+  end
+
+  defp handle_multi({:ok, %{create_user: user}}) do
+    {:ok, user}
   end
 end
